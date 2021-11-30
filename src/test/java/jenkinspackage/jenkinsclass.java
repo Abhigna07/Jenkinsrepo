465d12e6114b0340a35dbcb39172e2d6d1bcfc22
Abhigna Nakerekanti <abhignanakerekanti07@outlook.com>
2021-11-30 14:51:53 +0530

    Intial commit

---
+++ src/test/java/jenkinspackage/jenkinsclass.java
@@ -0,0 +1,10 @@
+package jenkinspackage;
+
+import org.testng.annotations.Test;
+
+public class jenkinsclass {
+  @Test
+  public void jenkinstest() {
+	  System.out.println("Jnekins maven testing");
+  }
+}
